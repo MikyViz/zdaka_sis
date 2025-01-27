80bf3549193f051ad7f7188579e989e45f5eabc8
MikyViz <vizenovsky@gmail.com>
2025-01-28 00:53:00 +0200

    rendering of user list for manager + CRUD

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useImperativeHandle, forwardRef } from 'react';
+import { useState, useEffect } from 'react';
 import {
   MDBRow,
   MDBCol,
@@ -13,6 +13,7 @@ import {
 
 const ContactForm = function (props) {
   const [formData, setFormData] = useState({
+    Id: '',
     firstName: '',
     lastName: '',
     country: '',
@@ -24,88 +25,110 @@ const ContactForm = function (props) {
     floor: '',
     cellPhone: '',
     number: '',
-    isActive: false, //TODO in DB it's not bool & it's named 'status'
+    isActive: false,
     instructions: '',
     additionalInfo: '',
   });
+
+  useEffect(() => {
+    if (props.user) {
+      setFormData({
+        Id: props.user.Id || '',
+        firstName: props.user.firstName || '',
+        lastName: props.user.lastName || '',
+        country: props.user.country || '',
+        city: props.user.city || '',
+        street: props.user.street || '',
+        houseNum: props.user.houseNum || '',
+        apt: props.user.apt || '',
+        email: props.user.email || '',
+        floor: props.user.floor || '',
+        cellPhone: props.user.cellPhone || '',
+        number: props.user.number || '',
+        isActive: props.user.isActive || false,
+        instructions: props.user.instructions || '',
+        additionalInfo: props.user.additionalInfo || '',
+      });
+    }
+  }, [props.user]);
 
   const handleChange = (e) => {
     const { id, value, checked, type } = e.target;
     setFormData((prevData) => ({
       ...prevData,
-      [id]: type === 'checkbox' ? checked : value,   
-    
+      [id]: type === 'checkbox' ? checked : value,
     }));
-    };
-    console.log(formData);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    props.req(formData);
+    // if (props.user)
+    //   props.req(props.user)
+    // else
+      props.req(formData);
   };
 
   return (
     <MDBContainer fluid className={`${props.styleClass} p-4 bg-light bg-gradient overflow-hidden`}>
       <form>
-        <MDBRow className="mb-3">
+        <MDBRow className="mb-3"> 
+          {/* TODO Добавить поле с обращением */}
           <MDBCol md="6">
-            <MDBInput id="firstName" label="First name" onChange={handleChange} />
+            <MDBInput id="firstName" label="First name" value={formData.firstName} onChange={handleChange} />
           </MDBCol>
           <MDBCol md="6">
-            <MDBInput id="lastName" label="Last name" onChange={handleChange} />
+            <MDBInput id="lastName" label="Last name" value={formData.lastName} onChange={handleChange} />
           </MDBCol>
         </MDBRow>
         <MDBRow className="mb-3">
           <MDBCol md="4">
-            <MDBInput id="country" label="Country" onChange={handleChange} />
+            <MDBInput id="country" label="Country" value={formData.country} onChange={handleChange} />
           </MDBCol>
           <MDBCol md="4">
-            <MDBInput id="city" label="City" onChange={handleChange} />
+            <MDBInput id="city" label="City" value={formData.city} onChange={handleChange} />
           </MDBCol>
-          {/*           // TODO: Add neighborhood form */}
+          {/* TODO: Add neighborhood form */}
           <MDBCol md="4">
-            <MDBInput id="street" label="Street" onChange={handleChange} />
+            <MDBInput id="street" label="Street" value={formData.street} onChange={handleChange} />
           </MDBCol>
         </MDBRow>
         <MDBRow className="mb-3">
           <MDBCol md="4">
-            <MDBInput id="houseNum" label="House Number" onChange={handleChange} />
+            <MDBInput id="houseNum" label="House Number" value={formData.houseNum} onChange={handleChange} />
           </MDBCol>
           <MDBCol md="4">
-            <MDBInput id="apt" label="Apartment Number" onChange={handleChange} />
+            <MDBInput id="apt" label="Apartment Number" value={formData.apt} onChange={handleChange} />
           </MDBCol>
           <MDBCol md="4">
-            <MDBInput id="floor" label="Floor" onChange={handleChange} />
+            <MDBInput id="floor" label="Floor" value={formData.floor} onChange={handleChange} />
           </MDBCol>
         </MDBRow>
         <MDBRow className="mb-3">
           <MDBCol md="6">
-            <MDBInput type="email" id="email" label="Email" onChange={handleChange} />
+            <MDBInput type="email" id="email" label="Email" value={formData.email} onChange={handleChange} />
           </MDBCol>
           <MDBCol md="6">
-            <MDBInput type="tel" id="cellPhone" label="Phone" onChange={handleChange} />
+            <MDBInput type="tel" id="cellPhone" label="Phone" value={formData.cellPhone} onChange={handleChange} />
           </MDBCol>
         </MDBRow>
         <MDBRow className="mb-3">
           <MDBCol md="3">
-          {/* //TODO repair */}
-            <MDBSwitch id="isActive" label="Is active" onChange={handleChange} /> 
+            <MDBSwitch id="isActive" label="Is active" checked={formData.isActive} onChange={handleChange} />
           </MDBCol>
           <MDBCol md="3" className="form-outline" data-mdb-input-init style={{ maxWidth: "150px" }}>
-            <MDBInput type="number" label="Number" id="number" className="form-control" onChange={handleChange} />
+            <MDBInput type="number" label="Number" id="number" className="form-control" value={formData.number} onChange={handleChange} />
           </MDBCol>
         </MDBRow>
-
-        <MDBInput className="mb-3" id="instructions" label="Instructions" onChange={handleChange} />
+        <MDBInput className="mb-3" id="instructions" label="Instructions" value={formData.instructions} onChange={handleChange} />
         <MDBInput
           wrapperClass="mb-4"
           type='textarea'
           id="additionalInfo"
           rows={4}
           label="Additional information"
+          value={formData.additionalInfo}
           onChange={handleChange}
         />
-
         <MDBBtn className="mb-4" block onClick={handleSubmit}>
           {props.btnText}
         </MDBBtn>
@@ -114,4 +137,4 @@ const ContactForm = function (props) {
   );
 };
 
-export default ContactForm
+export default ContactForm;
--- src/pages/ContactsList.jsx
+++ src/pages/ContactsList.jsx
@@ -1,14 +1,26 @@
 import React, { useState, useContext, useEffect } from 'react';
 import axios from 'axios';
+import { Modal, Button } from 'react-bootstrap'; 
+import ContactForm from '../components/ContactForm'
 import '../styles/contacts.css'
 import { GlobalStateContext } from "../GlobalStateProvider";
 
 export default function ContactsList() {
   const { globalState, setGlobalState, getData } = useContext(GlobalStateContext);
-
-  const [itsOK, setItsOk] = useState(false);
-  const [isntOK, setIsntsOk] = useState(false);
   const [list, setList] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const editPutReq = async (user) => {
+    try {
+      const putResponse = await axios.put(`http://localhost:8080/users/${user.Id}`,
+        user,
+        { headers: { 'authorization': `Bearer ${localStorage.getItem('token')}`, } })
+      console.log('Response from server:', putResponse?.data);}
+      catch (error) {
+        console.error('Error sending data to server:', error);
+      }
+    }
 
   useEffect(() => {
     if (!globalState.user || !globalState.org) { getData(); }
@@ -23,19 +35,53 @@ export default function ContactsList() {
             const usersOfOrgResponse = await axios.get(`http://localhost:8080/users/${orgUser.UserId}`,
               { headers: { 'authorization': `Bearer ${localStorage.getItem('token')}`, } }
             );
+            usersOfOrgResponse.data.show = false;
             newList.push(usersOfOrgResponse.data);
           }
           setList(newList);
           return newList;
         }
         catch (error) {
-          setIsntsOk(true);
           console.error('Error sending data to server:', error);
         }
       };
       allUsersReq();
     }
   }, [globalState.user, globalState.org])
+
+  const shower = (index) => {
+    setList(prevList => {
+      const newList = prevList.map((item, i) => {
+        if (i === index) { return { ...item, show: !item.show }; }
+        return item;
+      });
+      return newList;
+    });
+  };
+
+
+  const handleEditClick = (user) => {
+    setCurrentUser(user);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setCurrentUser(null);
+  };
+
+  const handleDeleteClick = async (userId) => {
+    try {
+      await axios.delete(`http://localhost:8080/users/${userId}`, {
+        headers: {
+          'authorization': `Bearer ${localStorage.getItem('token')}`,
+        },
+      });
+      setList(prevList => prevList.filter(user => user.Id !== userId));
+    } catch (error) {
+      console.error('Error deleting user:', error);
+    }
+  };
 
   return (
     <div className="d-flex flex-column align-items-center justify-content-center vh-100" style={{ background: 'rgb(175, 170, 211)' }}>
@@ -44,14 +90,57 @@ export default function ContactsList() {
       </div>}
       {globalState.user && (
         <div className="card-wrapper">
-        {list.map((user, index) => (
-          <div key={index} className="card p-3 m-3">
-            <h3>{user.degree || ''} {`${user.firstName} ${user.lastName}`}</h3>
-            <p>{user.email}</p>
-          </div>
+          {list.map((user, index) => (
+            <div key={index} className={`card p-3 m-3 ${user.show ? 'expanded' : ''}`} onClick={() => shower(index)}>
+              <div className="d-flex justify-content-between align-items-center">
+                <h3>{user.degree || ''} {`${user.firstName} ${user.lastName}`}
+                  {user.isGabbay && <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-warning">
+                    Gabbay
+                    <span className="visually-hidden">isGabbay</span>
+                  </span>}
+                </h3>
+                <div>
+                  <Button variant="link" onClick={(e) => { e.stopPropagation(); handleEditClick(user); }}>
+                    <i className="bi bi-pencil-square"></i>
+                  </Button>
+                  <Button variant="link" onClick={(e) => { e.stopPropagation(); handleDeleteClick(user.Id); }}>
+                    <i className="bi bi-trash"></i>
+                  </Button>
+                </div>
+              </div>
+              {user.show && <>{user.email && <p>{user.email}</p>}
+                {user.cellPhone && <p>{user.cellPhone}</p>}
+                {user.homePhone && <p>{user.homePhone}</p>}
+                {(user.street || user.houseNum || user.apt) && <p>{`${user.street || ''} ${user.houseNum || ''}/${user.apt || ''} ${user.flore || ''}${user.flore ? '-th flore' : ''}`}</p>}
+                {(user.neighborhood || user.city || user.country) && <p>{`${user.neighborhood || ''}, ${user.city || ''}, ${user.country || ''}`}</p>}
+                {user.instructions && <div className="overflow-y-scroll">{user.instructions}</div>}
+              </>}
+              <div className="d-flex flex-row align-items-center mt-2">
+                {user.isManager && <span className="badge bg-primary">Manager</span>}
+                {user.isActive && <span className="badge bg-success">Active</span>}
+                {!user.isActive && <span className="badge bg-danger">Inactive</span>}
+              </div>
+              {/* TODO add the num rendering */}
+
+            </div>
           )
           )}
         </div>
+      )}
+      {currentUser && (
+        <Modal show={showModal} onHide={handleCloseModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Edit User</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <ContactForm user={currentUser} btnText='Save Changes' req={editPutReq} />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleCloseModal}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
       )}
     </div>)
 };
